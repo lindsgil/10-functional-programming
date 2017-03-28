a7ee71ba46b983687a8887083061cd4d2ed53f65
Benjamin <ayzenbergb@gmail.com>
2017-03-28 11:14:07 -0700

    still can't get function

--- starter-code/public/scripts/article.js
+++ starter-code/public/scripts/article.js
@@ -59,7 +59,7 @@
 
   // TODO: Chain together a `map` and a `reduce` call to produce an array of unique author names.
   Article.allAuthors = () => {
-    return Article.all.map(article => article.author).reduce((authors, author) => {authors.indexOf(author) === -1 ? authors.push(author): null, []});
+    return Article.all.map(article => article.author).reduce((authors, author) => {authors.indexOf(author) === -1 ? authors.push(author): undefined, []});
   };
 
   Article.numWordsByAuthor = () => {
